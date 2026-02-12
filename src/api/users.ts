d401d575c918fb926fc72bbc8c61b9c978d6b531
Matheus <theus.sousaa@gmail.com>
2026-02-12 10:44:05 -0300

    UserSelector component finished for now

---
+++ src/api/users.ts
@@ -0,0 +1,8 @@
+import { client } from '../utils/fetchClient';
+import { User } from '../types/User';
+
+export const getUsers = () => {
+  return client.get<User[]>('/users');
+};
+
+// /comments/${id}
